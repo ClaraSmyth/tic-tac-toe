367d4d4ebc5be44a89db3a57514764f0f3c8c429
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-15 00:32:51 +0100

    Added a way to populate the gameboard

--- scipt.js
+++ scipt.js
@@ -1,8 +1,20 @@
 const gameBoard = (() => {
-    const board = [];
+    const board = () => ['x', 'x', 'x','o', 'o', 'o','x', 'x', 'x',];
+
+    return {board};
 })();
 
 const displayController = (() => {
+    const gameGrid = document.querySelector('.game-board');
+    const gridCube = document.querySelectorAll('.game-board-cube');
+
+    const populateGrid = () => {
+        gameBoard.board().forEach(item => {
+            gridCube.forEach(cube => cube.innerText = item);
+        });
+    };
+
+    return {populateGrid}
 
 })();
 
@@ -11,6 +23,6 @@ const Player = (name) => {
     return {sayName};
 };
 
-let test = Player('Test');
+console.log(gameBoard.board())
 
-jeff.sayName()
+displayController.populateGrid()
